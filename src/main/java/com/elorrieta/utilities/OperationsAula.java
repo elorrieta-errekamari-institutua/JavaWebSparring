f90879074acc6cc1c4823c2e7ecf7288aeed365b
MikelCursoElorrieta <curso.elorrieta.poo@gmail.com>
2021-05-25 10:16:33 +0200

    Añadidas operaciones aula

--- src/main/java/com/elorrieta/utilities/OperationsAula.java
+++ src/main/java/com/elorrieta/utilities/OperationsAula.java
@@ -1,7 +1,12 @@
 package com.elorrieta.utilities;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.elorrieta.modelo.dao.DAOAula;
 import com.elorrieta.modelo.pojo.Aula;
@@ -45,25 +50,67 @@ public class OperationsAula {
 
 	}
 
-	public static void insertAll(HttpServletRequest request, HttpServletResponse response, DAOAula daoAula) {
-		// TODO Auto-generated method stub
+	public static void insertAll(HttpServletRequest request, HttpServletResponse response, DAOAula daoAula)
+			throws IOException {
+		throw new IOException("Caracteristica sin implementar");
+	}
+
+	public static void delete(HttpServletRequest request, HttpServletResponse response, int id, DAOAula daoAula) {
+
+		try {
+
+			Aula aulaBorrada = daoAula.delete(id);
+			if (aulaBorrada != null) {
+				request.getRequestDispatcher("aula").forward(request, response);
+				System.out.println("Aula eliminado");
+			} else {
+				request.getRequestDispatcher("detalleAula.jsp").forward(request, response);
+				System.err.println("No se ha podido actualizar aula");
+			}
+		}
+
+		catch (Exception e) {
+			System.err.println("Id vacio");
+		}
 
 	}
 
-	public static void delete(HttpServletRequest request, HttpServletResponse response, Integer integer,
-			DAOAula daoAula) {
-		// TODO Auto-generated method stub
+	public static void select(HttpServletRequest request, HttpServletResponse response, int id, DAOAula daoAula)
+			throws ServletException, IOException {
+
+		Aula aula = null;
+		try {
+			aula = daoAula.getByid(id);
+		} catch (Exception e) {
+			System.err.println("Error recuperando aula");
+			e.printStackTrace();
+		}
+		HttpSession session = request.getSession();
+		if (aula != null) {
+			session.setAttribute("participante", aula);
+		}
+
+		request.getRequestDispatcher("detalleParticipante.jsp").forward(request, response);
 
 	}
 
-	public static void select(HttpServletRequest request, HttpServletResponse response, Integer integer,
-			DAOAula daoAula) {
-		// TODO Auto-generated method stub
+	public static void selectAll(HttpServletRequest request, HttpServletResponse response, DAOAula daoAula)
+			throws ServletException, IOException {
 
-	}
+		ArrayList<Aula> listaAulasDB = null;
+		try {
+			listaAulasDB = daoAula.getAll();
+		} catch (Exception e) {
+			System.err.println("Problemas recuperando participantes");
+			e.printStackTrace();
+		}
+		HttpSession session = request.getSession();
+		if (listaAulasDB != null) {
+			session.removeAttribute("listaAulas");
+			session.setAttribute("listaAulas", listaAulasDB);
+		}
 
-	public static void selectAll(HttpServletRequest request, HttpServletResponse response, DAOAula daoAula) {
-		// TODO Auto-generated method stub
+		request.getRequestDispatcher("cursos.jsp").forward(request, response);
 
 	}
 
